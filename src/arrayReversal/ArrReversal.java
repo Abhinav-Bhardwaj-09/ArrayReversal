060510aea2c7427d73440f109f45f87b9e78224d
abhinavbhardwaj <abhinavbhardwaj68@gmail.com>
2022-03-07 22:24:52 +0530

    Required logic

--- src/arrayReversal/ArrReversal.java
+++ src/arrayReversal/ArrReversal.java
@@ -1,5 +1,7 @@
 package arrayReversal;
 
+import java.util.Scanner;
+
 /**
  * Created by IntelliJ IDEA
  * User: Abhinav Bhardwaj
@@ -9,4 +11,36 @@ package arrayReversal;
 
 
 public class ArrReversal {
+
+    static void arrayReversal (int [] arr) {
+        int leftElement, rightIndex = arr.length;
+        for (int index = 0; index < arr.length/2; index++) {
+            leftElement = arr[index];
+            arr[index] = arr[--rightIndex];
+            arr[rightIndex] = leftElement;
+        }
+
+        System.out.print("\nReversed Array :  ");
+        for (int index = 0; index < arr.length; index++) {
+            System.out.print(arr[index] + " ");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nSize of the array: ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        System.out.print("\nElements of the array (space seperated): ");
+        for(int index = 0; index < size; index++) {
+            arr[index] = sc.nextInt();
+        }
+        arrayReversal(arr);
+
+    }
 }
